a94ae30e3a47ea24e17ad7bf18225397b9208d99
uino59 <43013722+uino59@users.noreply.github.com>
2021-08-24 14:00:24 +0100

    privatised error_messages dict in error_handler

--- error_handler.py
+++ error_handler.py
@@ -3,17 +3,17 @@ import asyncio
 
 class Error_handler():
     def __init__(self):
-        self.error_messages = {}
+        self.__error_messages = {}
 
     async def send_usr_error_message(self, user: int, message: str):
         error_msg = await user.send(message)
-        if user.id in self.error_messages:
-            self.error_messages[user.id].append(error_msg)
+        if user.id in self.__error_messages:
+            self.__error_messages[user.id].append(error_msg)
         else:
-            self.error_messages[user.id] = [error_msg]
+            self.__error_messages[user.id] = [error_msg]
 
     async def delete_usr_error_messages(self, user:int):
-        if user.id in self.error_messages:
-            del self.error_messages[user.id]
+        if user.id in self.__error_messages:
+            del self.__error_messages[user.id]
 
 
